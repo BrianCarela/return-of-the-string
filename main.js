d8c6aca20b30bcbda721170f741923e315864b46
BrianCarela <briancarela@gmail.com>
2021-02-19 11:15:38 -0500

    graded work

--- main.js
+++ main.js
@@ -4,8 +4,42 @@
 
 
 
+function yell(string) {
+  return string + "!";
+}
 
+function getFirstCharacter(string) {
+  return string[0];
+}
 
+function getLastCharacter(string) {
+  return string[string.length - 1];
+}
+
+function getOneCharacter(string, number) {
+  return string[number];
+}
+
+function getTwoCharacters(string, number1, number2) {
+  return string[number1] + string[number2];
+}
+
+function makeCapitalized(string) {
+  return string.toUpperCase();
+}
+
+function yellLouder(string) {
+  return string.toUpperCase() + "!!!";
+}
+
+function getInitials(string) {
+  const i1 = string[0];
+  const spaceIndex = string.indexOf(" ");
+  const i2 = string[spaceIndex + 1];
+  return i1 + "." + i2 + ".";
+
+  // return string[0] + "." + string[string.indexOf(" ") + 1] + ".";
+}
 
 /********************************************************************
  * THIS CODE IS FOR INTERNAL USE ONLY. DON'T CHANGE ANYTHING BELOW! *
